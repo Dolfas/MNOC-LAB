915b97f99b2b518ca8b95520d8641b5b4fd3eae0
Dolfas <rodolfo.amorim@tecnico.ulisboa.pt>
2024-02-18 20:13:31 +0000

    Updates to homework1 18/02/2024

--- Homework1.m
+++ Homework1.m
@@ -1,4 +1,5 @@
 %% Task 1
+clc
 load fp_lin_matrices_fit3.mat
 eigA = eig(A);
 fprintf('The eigenvalues of matrix A are: %d \n',eigA);
@@ -7,6 +8,7 @@ fprintf('The eigenvalues of matrix A are: %d \n',eigA);
 % invertible 
 
 %% Task 2
+clc
 CoMatrix = ctrb(A,B);
 rankCo = rank(CoMatrix);
 
@@ -16,27 +18,48 @@ fprintf('The rank of the controllability matrix is: %d \n',rankCo);
 % dimension of the state then the model is controllable 
 
 %% Task 3
+clc
+C1 = [0,0,1,0,0];
+ObsMatrix = obsv(A,C1);
+rankObs1 = rank(ObsMatrix);
+fprintf('The rank of the observability matrix when we only want x3 is: %d \n',rankObs1);
+%Comment: Since the observability matrix has rank lower than that of the
+%dimension of the state then the state realization is not observable 
 
+
+C = [1,0,0,0,0;0,1,0,0,0];
 ObsMatrix = obsv(A,C);
 rankObs = rank(ObsMatrix);
-fprintf('The rank of the observability matrix is: %d \n',rankObs);
-
+fprintf('The rank of the observability matrix when we want x1 and x3 is: %d \n',rankObs);
+%Comment: Since the observability matrix has rank equal to that of the
+%dimension of the state then the state realization is observable 
 
 %% Task 4
+clc
+[b,a] = ss2tf(A,B,C,D);
 
+%Output x1
+sys = tf(b(1,:),a);
+bode(sys, 'r--')
 
+%Output x3 
+sys2 =  tf(b(2,:),a);
+hold on; 
+bode(sys2, 'b--')
+hold on;
+legend('x1','x3')
 
 
 %% Task 5
-
+clc
 
 
 %% Task 6
-
+clc
 
 
 %% Task 7
-
+clc
 %% Task 8 
 
 
